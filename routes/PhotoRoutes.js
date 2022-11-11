bc308f457f37a5356dde5cf3c2690ccac3e7dfec
Davi Roque Maia de Sousa <sowdavi@gmail.com>
2022-11-11 00:12:00 -0300

    test routephoto

--- routes/PhotoRoutes.js
+++ routes/PhotoRoutes.js
@@ -14,7 +14,7 @@ const {imageUpload} = require("../middlewares/imageUpload")
 router.post("/", authGuard,imageUpload.single("image"),photoInsertValidation(),validate,insertPhoto);
 router.delete("/:id",authGuard, deletePhoto);
 router.get("/",authGuard,getAllPhotos);
-router.get("/user/:id", authGuard, getUserPhotos)
+router.get("/user/:id", getUserPhotos)
 router.get("/:id",authGuard, getPhotoById)
 router.put("/:id",authGuard, photoUpdateValidation(), validate ,updatePhoto)
 router.put("/like/:id",authGuard, likePhoto)
