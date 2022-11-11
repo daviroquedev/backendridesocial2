c3795ee5b92b1ddaf58676eb71d695ff90fc03f3
Davi Roque Maia de Sousa <sowdavi@gmail.com>
2022-11-11 15:50:49 -0300

    update photo controller

--- controllers/PhotoController.js
+++ controllers/PhotoController.js
@@ -93,33 +93,48 @@ const getPhotoById = async (req, res) => {
 
 }
 
-// update a photo
+// Update a photo
 const updatePhoto = async (req, res) => {
-    const { id } = req.params
-    const { title } = req.body
-
-    const reqUser = req.user
-    const photo = await Photo.findById(id)
-
-    //check if photo exist
+    const { id } = req.params;
+    const { title } = req.body;
+  
+    let image;
+  
+    if (req.file) {
+      image = req.file.filename;
+    }
+  
+    const reqUser = req.user;
+  
+    const photo = await Photo.findById(id);
+  
+    // Check if photo exists
     if (!photo) {
-        res.status(404).json({ errors: ["Foto não encontrada."] })
+      res.status(404).json({ errors: ["Foto não encontrada!"] });
+      return;
     }
-
-    //check if photo belongs to user
+  
+    // Check if photo belongs to user
     if (!photo.userId.equals(reqUser._id)) {
-        res.status(422).json({ errors: ["Ocorreu um erro, por favor tente novamente mais tarde."], });
-        return
+      res
+        .status(422)
+        .json({ errors: ["Ocorreu um erro, tente novamente mais tarde"] });
+      return;
     }
-
+  
     if (title) {
-        photo.title = title
+      photo.title = title;
     }
-
-    await photo.save()
-
-    res.status(200).json({ photo, message: "Foto atualizada com sucesso." })
-}
+  
+    if (image) {
+      photo.image = image;
+    }
+  
+    await photo.save();
+  
+    res.status(200).json({ photo, message: "Foto atualizada com sucesso!" });
+  };
+  
 
 // Like functionality
 const likePhoto = async (req, res) => {
