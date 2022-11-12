2c636650889ed0c246a9f8048a68a31c6759ee41
Davi Roque Maia de Sousa <sowdavi@gmail.com>
2022-11-12 12:20:10 -0300

    backend route updatePhoto

--- routes/PhotoRoutes.js
+++ routes/PhotoRoutes.js
@@ -2,22 +2,22 @@ const express = require("express")
 const router = express.Router()
 
 //controller
-const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto, likePhoto, commentPhoto} = require("../controllers/PhotoController")
+const { insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto, likePhoto, commentPhoto } = require("../controllers/PhotoController")
 
 //middlewares
-const {photoInsertValidation, photoUpdateValidation, commentValidation} = require("../middlewares/photoValidation")
+const { photoInsertValidation, photoUpdateValidation, commentValidation } = require("../middlewares/photoValidation")
 const authGuard = require("../middlewares/authGuard")
 const validate = require("../middlewares/handleValidation");
-const {imageUpload} = require("../middlewares/imageUpload")
+const { imageUpload } = require("../middlewares/imageUpload")
 
 //routes
-router.post("/", authGuard,imageUpload.single("image"),photoInsertValidation(),validate,insertPhoto);
-router.delete("/:id",authGuard, deletePhoto);
-router.get("/",authGuard,getAllPhotos);
+router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto);
+router.delete("/:id", authGuard, deletePhoto);
+router.get("/", authGuard, getAllPhotos);
 router.get("/user/:id", getUserPhotos)
-router.get("/:id",authGuard, getPhotoById)
-router.put("/:id",authGuard, photoUpdateValidation(), validate ,updatePhoto)
-router.put("/like/:id",authGuard, likePhoto)
-router.put("/comment/:id", authGuard,commentValidation(), validate, commentPhoto)
+router.get("/:id", authGuard, getPhotoById)
+router.put("/:id",authGuard,imageUpload.single("image"),photoUpdateValidation(),validate,updatePhoto);
+router.put("/like/:id", authGuard, likePhoto)
+router.put("/comment/:id", authGuard, commentValidation(), validate, commentPhoto)
 
 module.exports = router;
